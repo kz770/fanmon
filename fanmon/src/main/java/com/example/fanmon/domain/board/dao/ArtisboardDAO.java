37c64b382d787ee0c991e1599cbbf6d41ffcde4f
ccc700 <700eeu@gmail.com>
2024-09-25 16:25:58 +0900

    ✏️Modify: DAO 어노테이션 추가

--- fanmon/src/main/java/com/example/fanmon/domain/board/dao/ArtisboardDAO.java
+++ fanmon/src/main/java/com/example/fanmon/domain/board/dao/ArtisboardDAO.java
@@ -2,8 +2,10 @@ package com.example.fanmon.domain.board.dao;
 
 import com.example.fanmon.domain.board.entity.Artistboard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ArtisboardDAO extends JpaRepository<Artistboard, UUID> {
 }
