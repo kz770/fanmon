369b5dd58c06da606ed0891468bb7e1898da5b24
mochoi42 <principle950@naver.com>
2024-09-26 11:41:37 +0900

    ✏ï¸Modify: E Controller ì어노테이션 수정 - Controller 어노테이션 RestController 어노테이션으로 수정

--- fanmon/src/main/java/com/example/fanmon/domain/meeting/controller/StayroomController.java
+++ fanmon/src/main/java/com/example/fanmon/domain/meeting/controller/StayroomController.java
@@ -2,12 +2,9 @@ package com.example.fanmon.domain.meeting.controller;
 
 import com.example.fanmon.domain.meeting.service.StayroomService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class StayroomController {
     @Autowired
     StayroomService stayroomService;
--- fanmon/src/main/java/com/example/fanmon/domain/meeting/controller/StayuserlistController.java
+++ fanmon/src/main/java/com/example/fanmon/domain/meeting/controller/StayuserlistController.java
@@ -2,9 +2,9 @@ package com.example.fanmon.domain.meeting.controller;
 
 import com.example.fanmon.domain.meeting.service.StayuserlistService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class StayuserlistController {
     @Autowired
     private StayuserlistService stayuserlistService;
